b933db45aaf0ef8f532be9fc0866a68926229d70
Redth <jondick@gmail.com>
2014-04-16 11:27:24 -0400

    Added crypto classes from Mono

---
+++ Couchbase.Lite/Couchbase.Lite.Portable/Security/Locale.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace System.Security.Cryptography
+{
+	public class Locale
+	{
+		public Locale ()
+		{
+		}
+
+		public static string GetText(string text)
+		{
+			return text;
+		}
+	}
+}
+
